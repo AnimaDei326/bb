c2b3f143033ed8d4b800185da9871bf8bcf3fe8f
vem-itsolution <vem@it-solution.ru>
2019-02-08 20:21:35 +0300

    новая заглушка для аватарок, добавлен тестовый контент для услуг, интегрирована верстка в услугах с учетом последних правок из фронта

--- views/service.php
+++ views/service.php
@@ -11,104 +11,27 @@
             <!-- вот тут нужно будет менять фотку сверху или снизу -->
             <!-- получается во 2, 5, 8 (ф-ла 3n+2) блоках фотография находятся сверху, у остальных снизу -->
 
-            <!-- в data-service-id нужно также будет поместить счетчик --> 
+            <!-- в data-service-id нужно также будет поместить счетчик -->
 
-            <div class="services__block">
-                <div class="services__text-wrap" data-service-id="1">
-                    <h4 class="services-block__title">Финансовое моделирование и аудит моделей: </h4>
-                    <ul class="services__list">
-                        <li class="services-list__element">Разработка финансовых моделей инвесторов и развитие бизнеса</li>
-                        <li class="services-list__element">Экспертиза финансовых моделей</li>
-                        <li class="services-list__element">Оценка стоимости проектов и предприятий</li>
-                        <li class="services-list__element">Разработка ТЭО инвестиций и Бизнес-планов</li>
-                    </ul>
-                </div>
+            <? foreach ($this->params['services'] as $service):?>
 
-                <div class="service__img-block">
-                    <img src="/images/service1.jpg" alt="/images/service1.jpg" class="js-services__img" width="100%">
-                </div>
-            </div>
-
-            <div class="services__block">
-                <div class="service__img-block">
-                    <img src="/images/service1.jpg" alt="/images/service1.jpg" class="js-services__img" width="100%">
-                </div>
-
-                <div class="services__text-wrap" data-service-id="0">
-                    <h4 class="services-block__title">Финансовое моделирование и аудит моделей: </h4>
-                    <ul class="services__list">
-                        <li class="services-list__element">Разработка финансовых моделей инвесторов и развитие бизнеса</li>
-                        <li class="services-list__element">Экспертиза финансовых моделей</li>
-                        <li class="services-list__element">Оценка стоимости проектов и предприятий</li>
-                        <li class="services-list__element">Разработка ТЭО инвестиций и Бизнес-планов</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="services__block">
-                <div class="services__text-wrap" data-service-id="1">
-                    <h4 class="services-block__title">Финансовое моделирование и аудит моделей: </h4>
-                    <ul class="services__list">
-                        <li class="services-list__element">Разработка финансовых моделей инвесторов и развитие бизнеса</li>
-                        <li class="services-list__element">Экспертиза финансовых моделей</li>
-                        <li class="services-list__element">Оценка стоимости проектов и предприятий</li>
-                        <li class="services-list__element">Разработка ТЭО инвестиций и Бизнес-планов</li>
-                    </ul>
+                <div class="services__block">
+                    <div class="services__text-wrap" data-service-id="<?=$service['id']?>">
+                        <h4 class="services-block__title"><?=$service['name']?>: </h4>
+                        <ul class="services__list">
+                            <? foreach ($service['items'] as $item):?>
+                                <li class="services-list__element"><?=$item['name']?></li>
+                            <? endforeach;?>
+                        </ul>
+                    </div>
                 </div>
 
                 <div class="service__img-block">
-                    <img src="/images/service1.jpg" alt="/images/service1.jpg" class="js-services__img" width="100%">
-                </div>
-            </div>
-            
-            <div class="services__block">
-                <div class="services__text-wrap" data-service-id="1">
-                    <h4 class="services-block__title">Финансовое моделирование и аудит моделей: </h4>
-                    <ul class="services__list">
-                        <li class="services-list__element">Разработка финансовых моделей инвесторов и развитие бизнеса</li>
-                        <li class="services-list__element">Экспертиза финансовых моделей</li>
-                        <li class="services-list__element">Оценка стоимости проектов и предприятий</li>
-                        <li class="services-list__element">Разработка ТЭО инвестиций и Бизнес-планов</li>
-                    </ul>
+                    <img src="/images/<?=$service['picture']?>" alt="<?=$service['picture']?>" class="js-services__img" width="100%">
                 </div>
 
-                <div class="service__img-block">
-                    <img src="/images/service1.jpg" alt="/images/service1.jpg" class="js-services__img" width="100%">
-                </div>
-            </div>
+            <? endforeach;?>
 
-            <div class="services__block">
-                <div class="service__img-block">
-                    <img src="/images/service1.jpg" alt="/images/service1.jpg" class="js-services__img" width="100%">
-                </div>
-
-                <div class="services__text-wrap" data-service-id="0">
-                    <h4 class="services-block__title">Финансовое моделирование и аудит моделей: </h4>
-                    <ul class="services__list">
-                        <li class="services-list__element">Разработка финансовых моделей инвесторов и развитие бизнеса</li>
-                        <li class="services-list__element">Экспертиза финансовых моделей</li>
-                        <li class="services-list__element">Оценка стоимости проектов и предприятий</li>
-                        <li class="services-list__element">Разработка ТЭО инвестиций и Бизнес-планов</li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="services__block">
-                <div class="services__text-wrap" data-service-id="1">
-                    <h4 class="services-block__title">Финансовое моделирование и аудит моделей: </h4>
-                    <ul class="services__list">
-                        <li class="services-list__element">Разработка финансовых моделей инвесторов и развитие бизнеса</li>
-                        <li class="services-list__element">Экспертиза финансовых моделей</li>
-                        <li class="services-list__element">Оценка стоимости проектов и предприятий</li>
-                        <li class="services-list__element">Разработка ТЭО инвестиций и Бизнес-планов</li>
-                    </ul>
-                </div>
-
-                <div class="service__img-block">
-                    <img src="/images/service1.jpg" alt="/images/service1.jpg" class="js-services__img" width="100%">
-                </div>
-            </div>
-        </div>
         <!-- desctop version of services -->
 
 
@@ -120,8 +43,6 @@
             <!-- счетчик counter должен быть как в этом блоке и в том, что выше, я привязал его к -->
             <!-- полю data-service-id (у services_test-wrap) на 130 строчке -->
 
-            <? $counter = 0; ?>
-
             <? foreach ($this->params['services'] as $service):?>
 
                 <div class="services__block">
@@ -129,7 +50,7 @@
                         <img src="/images/<?=$service['picture']?>" alt="<?=$service['picture']?>" class="js-services__img">
                     </div>
 
-                    <div class="services__text-wrap" data-service-id="<?= $counter ?>">
+                    <div class="services__text-wrap" data-service-id="<?=$service['id']?>">
                         <h4 class="services-block__title"><?=$service['name']?>: </h4>
                         <ul class="services__list">
                             <? foreach ($service['items'] as $item):?>
@@ -138,9 +59,6 @@
                         </ul>
                     </div>
                 </div>
-
-                <? $counter++ ?>
-
             <? endforeach;?>
         </div>
         <!-- //mobile version of services -->
