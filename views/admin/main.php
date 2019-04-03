b467526fa8854e01853fb8dcfa4d796fbc6ff935
vem-itsolution <vem@it-solution.ru>
2019-04-04 01:18:22 +0300

    фронт админки

---
+++ views/admin/main.php
@@ -0,0 +1,12 @@
+<!-- Start: Topbar -->
+<header id="topbar" class="alt">
+    <div class="topbar-left">
+        <ol class="breadcrumb">
+            <li class="crumb-active">
+                <a href="/admin">Рабочий стол</a>
+            </li>
+        </ol>
+    </div>
+</header>
+<!-- End: Topbar -->
+Здесь будет что-то, что захотят видеть клиенты на своем рабочем столе. Например, календарь, курсы валют, кол-во необработанных заявок..
